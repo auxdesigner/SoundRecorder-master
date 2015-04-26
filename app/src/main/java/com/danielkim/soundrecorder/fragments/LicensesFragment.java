7641aa1af249acbb3c50742dbefc9e79328cb3d3
Liu Liu <lllxjy2009@gmail.com>
2015-04-25 18:20:09 -0700

    initial

---
+++ app/src/main/java/com/danielkim/soundrecorder/fragments/LicensesFragment.java
@@ -0,0 +1,11 @@
+package com.danielkim.soundrecorder.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.danielkim.soundrecorder.R;
+
